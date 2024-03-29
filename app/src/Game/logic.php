e49a94b141a7f2d3b02a2f00f41d545867c8c8ee
milano1110 <milanschuringa@gmail.com>
2024-03-29 22:44:37 +0100

    fixed #3 and added tests

--- app/src/Game/logic.php
+++ app/src/Game/logic.php
@@ -31,15 +31,97 @@ class Logic
         }
     }
 
+    public function validMove(Player $player, $to, $from)
+    {
+        $tile = $this->move($player, $to, $from);
+        if ($tile) {
+            if ($this->board->isOccupied($from)) {
+                $this->board->pushTile($from, $tile[1], $tile[0]);
+            } else {
+                $this->board->setTile($from, $tile[1], $tile[0]);
+            }
+        }
+    }
+
+    public function getValidPositionsPlay(Player $player)
+    {
+        $to = [];
+        $offsets = $this->board->getOffsets();
+        $positions = array_keys($this->board->getBoard());
+        foreach ($offsets as $pq) {
+            foreach ($positions as $pos) {
+                $pq2 = explode(',', $pos);
+                $res = ($pq[0] + $pq2[0]) . ',' . ($pq[1] + $pq2[1]);
+                try {
+                    $this->getValidPositions($player, $res);
+                } catch (Exception $e) {
+                    continue;
+                }
+                $to[] = $res;
+            }
+        }
+        return array_unique($to);
+    }
+
+    public function getValidPositionsMove(Player $player)
+    {
+        $tiles = $this->board->getTiles($player);
+        $pieces = $this->board->getPieces($player);
+        $to = [];
+        $offsets = $this->board->getOffsets();
+
+        foreach ($pieces as $piece) {
+            foreach ($tiles as $tile) {
+                foreach ($offsets as $pq) {
+                    $pq2 = explode(',', $tile);
+                    $res = ($pq[0] + $pq2[0]) . ',' . ($pq[1] + $pq2[1]);
+                    try {
+                        $this->validMove($player, $res, $tile, $piece);
+                        $to[] = $res;
+                    } catch (Exception $e) {
+                        continue;
+                    }
+                }
+            }
+        }
+        return array_unique($to);
+    }
+
+    private function doesHiveSplit($to)
+    {
+        $board = $this->board;
+        if (!$board->hasNeighbour($to)) {
+            throw new Exception("Move would split hive");
+        }
+        $all = $board->getKeys();
+        $queue = [array_shift($all)];
+        while ($queue) {
+            $next = explode(',', array_shift($queue));
+            foreach (Board::getOffsets() as $pq) {
+                list($p, $q) = $pq;
+                $p += $next[0];
+                $q += $next[1];
+                if (in_array("$p,$q", $all)) {
+                    $queue[] = "$p,$q";
+                    $all = array_diff($all, ["$p,$q"]);
+                }
+            }
+        }
+        if ($all) {
+            throw new Exception("Move would split hive");
+        }
+    }
+
     public function play(Player $player, $piece, $to)
     {
         $hand = $player->getHand();
         $this->getValidPositions($player, $to);
         if (!$hand->handHasPiece($piece)) {
             throw new Exception("Player does not have tile");
-        } elseif ($hand->handSize() <= 8 && $hand->handHasTile("Q")) {
+        } elseif ($hand->handSize() <= 8 && $hand->handHasTile("Q") && $piece != "Q") {
             throw new Exception("Must play queen bee");
         }
+        return true;
     }
 
     public function move(Player $player, $to, $from)
@@ -53,37 +135,19 @@ class Logic
                 throw new Exception("Tile is not owned by player");
             } elseif ($player->getHand()->handHasTile("Q")) {
                 throw new Exception("Queen bee is not played");
-            } else {
-                $tile = $board->popTile($from);
-                if (!$board->hasNeighbour($to)) {
-                    throw new Exception("Move would split hive");
-                }
-                $all = $board->getKeys();
-                $queue = [array_shift($all)];
-                while ($queue) {
-                    $next = explode(',', array_shift($queue));
-                    foreach (Board::getOffsets() as $pq) {
-                        list($p, $q) = $pq;
-                        $p += $next[0];
-                        $q += $next[1];
-                        if (in_array("$p,$q", $all)) {
-                            $queue[] = "$p,$q";
-                            $all = array_diff($all, ["$p,$q"]);
-                        }
-                    }
-                }
-                if ($all) {
-                    throw new Exception("Move would split hive");
-                } else {
-                    if ($from == $to) {
-                        throw new Exception("Tile must move");
-                    } elseif ($board->isOccupied($to) && $tile[1] != "B") {
-                        throw new Exception("Tile not empty");
-                    } elseif ($tile[1] == "Q" || $tile[1] == "B") {
-                        if (!$board->slide($from, $to)) {
-                            throw new Exception("Tile must slide");
-                        }
-                    }
+            }
+            $tile = $board->popTile($from);
+            $player_pieces = array_keys($player->getHand()->getAvailablePieces());
+            if (count($player_pieces) != 4 && !isset($player_pieces["Q"])) {
+                $this->doesHiveSplit($to);
+            }
+            if ($from == $to) {
+                throw new Exception("Tile must move");
+            } elseif ($board->isOccupied($to) && $tile[1] != "B") {
+                throw new Exception("Tile not empty");
+            } elseif ($tile[1] == "Q" || $tile[1] == "B") {
+                if (!$board->slide($from, $to)) {
+                    throw new Exception("Tile must slide");
                 }
             }
         } catch (Exception $e) {
@@ -91,7 +155,7 @@ class Logic
                 if ($board->isOccupied($from)) {
                     $board->pushTile($from, $tile[1], $tile[0]);
                 } else {
-                    $board[$from] = [$tile];
+                    $board->setTile($from, $tile[1], $tile[0]);
                 }
             }
             throw $e;
---
+++ app/tests/LogicTest.php
@@ -0,0 +1,80 @@
+<?php
+
+require_once __DIR__ . '/../src/Game/Logic.php';
+require_once __DIR__ . '/../src/Game/Board.php';
+require_once __DIR__ . '/../src/Game/Hand.php';
+require_once __DIR__ . '/../src/Game/Player.php';
+// require_once __DIR__ . '/../src/Entity/Database.php';
+
+use PHPUnit\Framework\TestCase;
+use App\Game\Logic;
+use App\Game\Board;
+use App\Entity\Database;
+use App\Game\Hand;
+use App\Game\Player;
+
+class LogicTest extends TestCase
+{
+
+    public function testUndo()
+    {
+        // Mock the Database class
+        $databaseMock = Mockery::mock('overload:App\Entity\Database');
+        $databaseMock->shouldReceive('getMove')
+            ->with('A1')
+            ->andReturn(['id' => 1, 'state' => 'state1', 'previous_id' => 'A0']);
+        $databaseMock->shouldReceive('deleteMove')
+            ->with(1)
+            ->andReturn(true);
+
+        // Mock the Board class
+        $boardMock = Mockery::mock('overload:App\Game\Board');
+        $boardMock->shouldReceive('getBoard')
+            ->andReturn(['A1' => 'X']);
+        $boardMock->shouldReceive('setState')
+            ->with('state1');
+
+        $logic = new Logic($boardMock);
+        $previous_id = $logic->undo('A1');
+
+        $this->assertEquals('A0', $previous_id);
+    }
+
+    public function tearDown(): void
+    {
+        Mockery::close();
+    }
+
+    public function testPlayQueenMoveFour()
+    {
+        $board = new Board([
+            '0,0' => [[0, 'S']],
+            '0,1' => [[1, 'S']],
+            '0,-1' => [[0, 'A']],
+            '0,2' => [[1, 'A']],
+            '0,-2' => [[0, 'B']],
+            '0,3' => [[1, 'B']]
+        ]);
+        $hand = new Hand(["Q" => 1, "B" => 1, "S" => 1, "A" => 2, "G" => 3]);
+        $logic = new Logic($board);
+        $player = new Player(0, $hand, $board);
+        $this->assertTrue($logic->play($player, 'Q', '0,-3'));
+    }
+
+    public function testNoQueenMoveFour()
+    {
+        $board = new Board([
+            '0,0' => [[0, 'S']],
+            '0,1' => [[1, 'S']],
+            '0,-1' => [[0, 'A']],
+            '0,2' => [[1, 'A']],
+            '0,-2' => [[0, 'B']],
+            '0,3' => [[1, 'B']]
+        ]);
+        $hand = new Hand(["Q" => 1, "B" => 1, "S" => 1, "A" => 2, "G" => 3]);
+        $logic = new Logic($board);
+        $player = new Player(0, $hand, $board);
+        $this->expectExceptionMessage('Must play queen bee');
+        $logic->play($player, 'B', '0,-3');
+    }
+}
